c138e8f29c82dc842005447fcc90020f36db944c
michael hohl <mhohl@rocketmail.com>
2018-05-16 21:34:53 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require_relative 'config/environment'
+require_relative 'models/courses.rb'
+require_relative 'models/students.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :new
+  end
+
+  post '/student' do
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
+
+    @courses = Course.all
+
+    erb :student
+  end
 
 end
